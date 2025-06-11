a0cddc2949fe78b01a5ccbfb7e77c92295f40bde
yun10013 <ljj2763713736@163.com>
2025-06-11 23:12:50 +0800

    Signed-off-by: yun10013 <ljj2763713736@163.com>

---
+++ C#/5.C#高级/LINQ/LINQ 相等运算符 SequenceEqual/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace LINQ_相等运算符_SequenceEqual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ_相等运算符_SequenceEqual");
+
+            //只有一个相等运算符：SequenceEqual。
+            //SequenceEqual方法检查两个集合中的元素数量，每个元素的值和元素的顺序是否相等。
+            //bool SequenceEqual<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second);
+            //bool SequenceEqual<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second, IEqualityComparer<TSource>? comparer);
+            
+            //如果集合包含原始数据类型的元素，则它将比较元素的值和数量，而具有复杂类型元素的集合将检查对象的引用。
+            //因此，如果对象具有相同的引用，则将它们视为相等，否则将其视为不相等。
+
+            //下面的示例演示了带有原始数据类型集合的SequenceEqual方法。
+            IList<string> strList1 = new List<string>() { "One", "Two", "Three", "Four", "Three" };
+
+            IList<string> strList2 = new List<string>() { "One", "Two", "Three", "Four", "Three" };
+
+            bool isEqual = strList1.SequenceEqual(strList2); // 返回true
+            Console.WriteLine(isEqual);
+            //输出：
+            //True
+
+            //如果元素的顺序不同，则SequenceEqual()方法返回false。
+            IList<string> strList3= new List<string>() { "Two", "One", "Three", "Four", "Three" };
+
+            bool isEqual1 = strList1.SequenceEqual(strList3); // 返回false
+            Console.WriteLine(isEqual1);
+            //输出：
+            //False
+
+            //SequenceEqual扩展方法检查两个对象的引用，以确定两个序列是否相等。这可能会给出错误的结果。看以下示例： 
+            Student std = new Student() { StudentID = 1, StudentName = "Bill" };
+
+            IList<Student> studentList1 = new List<Student>() { std };
+
+            IList<Student> studentList2 = new List<Student>() { std };
+
+            bool isEqual2 = studentList1.SequenceEqual(studentList2); // 返回true
+
+            Student std1 = new Student() { StudentID = 1, StudentName = "Bill" };
+            Student std2 = new Student() { StudentID = 1, StudentName = "Bill" };
+
+            IList<Student> studentList3 = new List<Student>() { std1 };
+
+            IList<Student> studentList4 = new List<Student>() { std2 };
+
+            isEqual = studentList3.SequenceEqual(studentList4);// 返回false
+            //在上面的示例中，studentList1和studentList2包含相同的学生对象std。
+            //因此studentList1.SequenceEqual(studentList2)返回true。
+            //但是，stdList1和stdList2包含两个单独的学生对象std1和std2。
+            //所以现在，即使std1和std2包含相同的值，stdList1.SequenceEqual(stdList2)也将返回false。
+
+            //现在，您可以使用SequenceEqual扩展方法中的上述StudentComparer类作为第二个参数来比较值：
+            //示例：C＃使用SequenceEqual比较对象类型元素
+            IList<Student> studentList5 = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 19 }
+            };
+
+            IList<Student> studentList6 = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 19 }
+            };
+            // 以下返回true
+            bool isEqual3 = studentList5.SequenceEqual(studentList6, new StudentComparer());
+
+            //要记住的要点
+            //  SequenceEqual方法比较项目数及其原始数据类型的值。
+            //  SequenceEqual方法比较对象对复杂数据类型的引用。
+            //  如果要比较两个复杂类型集合的值使用IEqualityComparer类可通过SequenceEqual方法比较两个复杂类型的集合。
+        }
+    }
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
+    //要比较两个复杂类型（引用类型或对象）集合的值，需要实现IEqualityComperar<T> 接口，如下所示。
+    class StudentComparer : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y)
+        {
+            if (x.StudentID == y.StudentID && x.StudentName.ToLower() == y.StudentName.ToLower())
+                return true;
+
+            return false;
+        }
+
+        public int GetHashCode(Student obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
---
+++ C#/5.C#高级/LINQ/LINQ 限定运算符 Contains/Program.cs
@@ -0,0 +1,79 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LINQ_限定运算符_Contains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ_限定运算符_Contains\r\n{");
+            //Contains运算符检查集合中是否存在指定的元素，并返回布尔值。
+
+            //Contains()扩展方法有以下两个重载。
+            //第一个重载方法需要一个值来检入集合，
+            //第二个重载方法需要使用IEqualityComparer类型的附加参数来进行自定义相等性比较。
+            //bool Contains<TSource>(this IEnumerable<TSource> source, TSource value);
+            //bool Contains<TSource>(this IEnumerable<TSource> source, TSource value, IEqualityComparer<TSource>? comparer);
+
+            //如上所述，Contains()扩展方法需要一个值作为输入参数进行检查。
+            //值的类型必须与泛型集合的类型相同。下面的示例包含检查集合中是否存在10。请注意，int是泛型集合的一种类型。
+
+            IList<int> intList = new List<int>() { 1, 2, 3, 4, 5 };
+            bool result = intList.Contains(10);  // 返回 false
+
+            //上面的示例适用于原始数据类型。但是，它不适用于自定义类。看以下示例：
+            List<Student> studentList = new List<Student>() {
+               new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+               new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 } ,
+               new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+               new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+               new Student() { StudentID = 5, StudentName = "Ron" , Age = 19 }
+            };
+            //tudent std = new Student() { StudentID = 3, StudentName = "Bill" };
+            //ool result = studentList.Contains(std); //returns false  报错
+
+            //正如您在上面的示例中看到的，Contains返回false，即使studentList中存在“Bill”。
+            //这是因为Contains扩展方法只比较对象的引用，而不比较对象的实际值。
+            //所以要比较student对象的值，需要通过实现IEqualityComparer接口创建一个类，该接口比较两个student对象的值并返回boolean。
+
+            //现在，你可以在Contains扩展方法的第二个重载方法中使用上面的StudentComparer类，
+            //该方法接受第二个参数为IEqualityComparer类型，如下所示:
+
+            Student std = new Student() { StudentID = 3, StudentName = "Bill" };
+            bool result1 = studentList.Contains(std, new StudentComparer()); //returns true
+                                                                             //因此，必须使用Comparer类才能从自定义类的Contains扩展方法中获得正确的结果。
+
+            //注意事项
+            //  All, Any & Contains是LINQ中的限定运算符。
+            //  All检查序列中的所有元素是否满足指定条件。
+            //  检查序列中是否有任何元素满足指定条件
+            //  Contains 操作符检查指定的元素是否存在于集合中。
+            //  使用派生 IEqualityOperator 和 Contains 的自定义类检查集合中的对象。
+            //  在C#或VB.Net的查询语法中不支持All, Any & Contains。
+        }
+    }
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
+    //以下是StudentComparer类，它实现IEqualityComparer<Student> 接口来比较两个Students对象的值：
+    class StudentComparer : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y)
+        {
+            if (x.StudentID == y.StudentID &&
+                        x.StudentName.ToLower() == y.StudentName.ToLower())
+                return true;
+
+            return false;
+        }
+
+        public int GetHashCode(Student obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
